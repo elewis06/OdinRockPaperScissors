e5164192ca1262df4383ebf0129c18251058ecc7
Ethan Lewis <ethanjustis@gmail.com>
2025-01-23 07:42:17 +0800

    Add user-interface and game logic to UI.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,75 @@
 const ROCK = "ROCK", PAPER = "PAPER", SCISSORS = "SCISSORS";
+const WIN = "WIN", TIE = "TIE", LOSE = "LOSE";
+let userWins = 0;
+let userLosses = 0;
+let computerWins = 0;
+let computerLosses = 0;
+let ties = 0;
+
+function resetScore() {
+    userWins = 0;
+    userLosses = 0;
+    computerWins = 0;
+    computerLosses = 0;
+    ties = 0;
+
+    resetRoundResultText();
+    updateScoreBoardText();
+}
+
+function incrementUserWins() {
+    userWins += 1;
+}
+
+function incrementUserLosses() {
+    userLosses += 1;
+}
+
+function incrementComputerWins() {
+    computerWins += 1;
+}
+
+function incrementComputerLosses() {
+    computerLosses += 1;
+}
+
+function incrementTies() {
+    ties += 1;
+}
+
+function setRoundResultText(userSelection, computerSelection, roundResult) {
+    let welcomeMessageDiv = document.querySelector(".welcome-message");
+    let userSelectionMessageDiv = document.querySelector(".user-selection-message");
+    let computerSelectionMessageDiv = document.querySelector(".computer-selection-message");
+    let resultMessageDiv = document.querySelector(".result-message");
+
+    welcomeMessageDiv.textContent = "";
+    userSelectionMessageDiv.textContent = `You selected ${userSelection}.`;
+    computerSelectionMessageDiv.textContent = `Computer selected ${computerSelection}.`;
+    resultMessageDiv.textContent = `You ${roundResult}!`;
+}
+
+function resetRoundResultText() {
+    let welcomeMessageDiv = document.querySelector(".welcome-message");
+    let userSelectionMessageDiv = document.querySelector(".user-selection-message");
+    let computerSelectionMessageDiv = document.querySelector(".computer-selection-message");
+    let resultMessageDiv = document.querySelector(".result-message");
+
+    welcomeMessageDiv.textContent = "Welcome to a game of rock, paper, scissors! Make your selection!";
+    userSelectionMessageDiv.textContent = "";
+    computerSelectionMessageDiv.textContent = "";
+    resultMessageDiv.textContent = "";
+}
+
+function updateScoreBoardText() {
+    let userWinsScoreDiv = document.querySelector(".user-wins-score");
+    let userLossesScoreDiv = document.querySelector(".user-losses-score");
+    let tiesScoreDiv = document.querySelector(".ties-score");
+
+    userWinsScoreDiv.textContent = userWins;
+    userLossesScoreDiv.textContent = userLosses;
+    tiesScoreDiv.textContent = ties;
+}
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, or Scissors?").toUpperCase();
@@ -8,73 +79,71 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function getComputerChoice() {
+function getComputerSelection() {
     return [ROCK, PAPER, SCISSORS][Math.floor(Math.random() * 3)];
 }
 
-function getRoundResult(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        return 0;
+function getRoundResult(humanChoice, computerSelection) {
+    if (humanChoice === computerSelection) {
+        return TIE;
     } else {
         if (
-            humanChoice === ROCK && computerChoice === SCISSORS ||
-            humanChoice === PAPER && computerChoice === ROCK ||
-            humanChoice === SCISSORS && computerChoice === PAPER
+            humanChoice === ROCK && computerSelection === SCISSORS ||
+            humanChoice === PAPER && computerSelection === ROCK ||
+            humanChoice === SCISSORS && computerSelection === PAPER
         ) {
-            return 1;
+            return WIN;
         } else if (
-            humanChoice === ROCK && computerChoice === PAPER ||
-            humanChoice === PAPER && computerChoice === SCISSORS ||
-            humanChoice === SCISSORS && computerChoice === ROCK
+            humanChoice === ROCK && computerSelection === PAPER ||
+            humanChoice === PAPER && computerSelection === SCISSORS ||
+            humanChoice === SCISSORS && computerSelection === ROCK
         ) {
-            return -1;
+            return LOSE;
         }
     }
 }
 
-function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    let roundResult = getRoundResult(humanChoice, computerChoice);
+function playRound(e) {
+    e.stopPropagation();
+    let userSelectedButton = e.target.name;
+    let computerSelection = getComputerSelection();
+    let roundResult = "";
+    let userSelection = "";
 
-    console.log(`You play ${humanChoice}. Computer plays ${computerChoice}.`);
+    if (userSelectedButton === "userRockButton") {
+        roundResult = getRoundResult(ROCK, computerSelection);
+        userSelection = ROCK;
+    } else if (userSelectedButton === "userPaperButton") {
+        roundResult = getRoundResult(PAPER, computerSelection);
+        userSelection = PAPER;
+    } else if (userSelectedButton === "userScissorsButton") {
+        roundResult = getRoundResult(SCISSORS, computerSelection);
+        userSelection = SCISSORS;
+    } else {
+        console.error(`Something went wrong. Expected value of [userRockButton|userPaperButton|userScissorsButton]. Received:\n${e}`);
+        return -1;
+    }
 
     switch(roundResult) {
-        case 0:
-            console.log(`${humanChoice} matches ${computerChoice}. No winner!`);
+        case WIN:
+            incrementUserWins();
+            incrementComputerLosses();
             break;
-        case 1:
-            console.log(`${humanChoice} beats ${computerChoice}. You win!`);
+        case LOSE:
+            incrementComputerWins();
+            incrementUserLosses();
             break;
-        case -1:
-            console.log(`${computerChoice} beats ${humanChoice}. You lose!`);
+        case TIE:
+            incrementTies();
             break;
     }
 
-    return roundResult;
+    setRoundResultText(userSelection, computerSelection, roundResult);
+    updateScoreBoardText();
 }
 
-function playGame() {
-    let numRounds = 5;
-    let humanScore = 0;
-    let computerScore = 0;
+let userOptionsButtons = document.querySelectorAll(".user-option");
+let resetScoreButton = document.querySelector(".reset-score-button");
 
-    for (let round = 1; round <= numRounds; round++) {
-        let roundResult = playRound();
-        if (roundResult == 1) {
-            humanScore++;
-        } else if (roundResult == -1) {
-            computerScore++;
-        }
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}. You Win!`);
-    } else if (humanScore < computerScore) {
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}. You lost!`);
-    } else {
-        console.log(`Your score: ${humanScore}. Computer score: ${computerScore}. You tied!`);
-    }
-}
-
-playGame();
+userOptionsButtons.forEach((userOptionsButton) => userOptionsButton.addEventListener("click", playRound));
+resetScoreButton.addEventListener("click", resetScore);
